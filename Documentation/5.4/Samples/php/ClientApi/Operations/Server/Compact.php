cfcb20c9a83b3852bf5bbccf96a1506660966f58
Aleksandar Sabo <alxsabo@gmail.com>
2024-10-28 14:17:29 +0100

    Client API > Operations > Server Wide > Compact database

--- Documentation/5.4/Samples/php/ClientApi/Operations/Server/Compact.php
+++ Documentation/5.4/Samples/php/ClientApi/Operations/Server/Compact.php
@@ -2,133 +2,146 @@
 
 namespace RavenDB\Samples\ClientApi\Operations\Server;
 
+use RavenDB\Constants\PhpClient;
 use RavenDB\Documents\DocumentStore;
+use RavenDB\Documents\Operations\Indexes\GetIndexNamesOperation;
 use RavenDB\Documents\Operations\Operation;
+use RavenDB\Documents\Operations\OperationIdResult;
+use RavenDB\Documents\Operations\CompactDatabaseOperation;
+use RavenDB\ServerWide\CompactSettings;
+use RavenDB\ServerWide\DatabaseRecordWithEtag;
+use RavenDB\ServerWide\GetDatabaseRecordOperation;
+use RavenDB\Type\StringArrayResult;
 
 class Compact
 {
     public function samples(): void
     {
         $documentStore = new DocumentStore();
-//        try {
-//            #region compact_0
-//
-//            // Define the compact settings
-//            $settings = new CompactSettings();
-//            $settings->setDatabaseName("Northwind");
-//            // Set 'Documents' to true to compact all documents in database
-//            // Indexes are not set and will not be compacted
-//            $settings->setDocuments(true);
-//
-//
-//            // Define the compact operation, pass the settings
-//            /** @var IServerOperation<OperationIdResult>  $compactOp */
-//            $compactOp = new CompactDatabaseOperation($settings);
-//
-//            // Execute compaction by passing the operation to Maintenance.Server.Send
-//            /** @var Operation $operation */
-//            $operation = $documentStore->maintenance()->server()->send($compactOp);
-//
-//            // Wait for operation to complete, during compaction the database is offline
-//            $operation->waitForCompletion();
-//            #endregion
-//        } finally {
-//            $documentStore->close();
-//        }
+        try {
+            #region compact_0
 
-//        using (var documentStore = new DocumentStore())
-//        {
-//            #region compact_1
-//            // Define the compact settings
-//            CompactSettings settings = new CompactSettings
-//            {
-//                // Database to compact
-//                DatabaseName = "Northwind",
-//
-//                // Setting 'Documents' to false will compact only the specified indexes
-//                Documents = false,
-//
-//                // Specify which indexes to compact
-//                Indexes = new[] { "Orders/Totals", "Orders/ByCompany" },
-//
-//                // Optimize indexes is Lucene's feature to gain disk space and efficiency
-//                // Set whether to skip this optimization when compacting the indexes
-//                SkipOptimizeIndexes = false
-//            };
-//
-//            // Define the compact operation, pass the settings
-//            IServerOperation<OperationIdResult> compactOp = new CompactDatabaseOperation(settings);
-//
-//            // Execute compaction by passing the operation to Maintenance.Server.Send
-//            Operation operation = documentStore.Maintenance.Server.Send(compactOp);
-//            // Wait for operation to complete
-//            operation.WaitForCompletion();
-//            #endregion
-//        }
-//
-//        using (var documentStore = new DocumentStore())
-//        {
-//            #region compact_2
-//            // Get all indexes names in the database using the 'GetIndexNamesOperation' operation
-//            // Use 'ForDatabase' if the target database is different than the default database defined on the store
-//            string[] allIndexNames =
-//                documentStore.Maintenance.ForDatabase("Northwind")
-//                    .Send(new GetIndexNamesOperation(0, int.MaxValue));
-//
-//            // Define the compact settings
-//            CompactSettings settings = new CompactSettings
-//            {
-//                DatabaseName = "Northwind", // Database to compact
-//
-//                Documents = true,           // Compact all documents
-//
-//                Indexes = allIndexNames,    // All indexes will be compacted
-//
-//                SkipOptimizeIndexes = true  // Skip Lucene's indexes optimization
-//            };
-//
-//            // Define the compact operation, pass the settings
-//            IServerOperation<OperationIdResult> compactOp = new CompactDatabaseOperation(settings);
-//
-//            // Execute compaction by passing the operation to Maintenance.Server.Send
-//            Operation operation = documentStore.Maintenance.Server.Send(compactOp);
-//            // Wait for operation to complete
-//            operation.WaitForCompletion();
-//            #endregion
-//        }
-//
-//        using (var documentStore = new DocumentStore())
-//        {
-//            #region compact_3
-//            // Get all member nodes in the database-group using the 'GetDatabaseRecordOperation' operation
-//            List<string> allMemberNodes =
-//                documentStore.Maintenance.Server.Send(new GetDatabaseRecordOperation("Northwind"))
-//                    .Topology.Members;
-//
-//            // Define the compact settings as needed
-//            CompactSettings settings = new CompactSettings
-//            {
-//                // Database to compact
-//                DatabaseName = "Northwind",
-//
-//                //Compact all documents in database
-//                Documents = true
-//            };
-//
-//            // Execute the compact operation on each member node
-//            foreach (string nodeTag in allMemberNodes)
-//            {
-//                // Define the compact operation, pass the settings
-//                IServerOperation<OperationIdResult> compactOp = new CompactDatabaseOperation(settings);
-//
-//                // Execute the operation on a specific node
-//                // Use `ForNode` to specify the node to operate on
-//                Operation operation = documentStore.Maintenance.Server.ForNode(nodeTag).Send(compactOp);
-//                // Wait for operation to complete
-//                operation.WaitForCompletion();
-//            }
-//            #endregion
-//        }
+            // Define the compact settings
+            $settings = new CompactSettings();
+            $settings->setDatabaseName("Northwind");
+            // Set 'Documents' to true to compact all documents in database
+            // Indexes are not set and will not be compacted
+            $settings->setDocuments(true);
+
+
+            // Define the compact operation, pass the settings
+            /** @var OperationIdResult  $compactOp */
+            $compactOp = new CompactDatabaseOperation($settings);
+
+            // Execute compaction by passing the operation to Maintenance.Server.Send
+            /** @var Operation $operation */
+            $operation = $documentStore->maintenance()->server()->send($compactOp);
+
+            // Wait for operation to complete, during compaction the database is offline
+            $operation->waitForCompletion();
+            #endregion
+        } finally {
+            $documentStore->close();
+        }
+
+        $documentStore = new DocumentStore();
+        try {
+            #region compact_1
+            // Define the compact settings
+            $settings = new CompactSettings();
+
+            // Database to compact
+            $settings->setDatabaseName("Northwind");
+
+            // Setting 'Documents' to false will compact only the specified indexes
+            $settings->setDocuments(false);
+
+            // Specify which indexes to compact
+            $settings->setIndexes([ "Orders/Totals", "Orders/ByCompany" ]);
+
+            // Optimize indexes is Lucene's feature to gain disk space and efficiency
+            // Set whether to skip this optimization when compacting the indexes
+            $settings->setSkipOptimizeIndexes(false);
+
+
+            // Define the compact operation, pass the settings
+            /** @var OperationIdResult $compactOp */
+            $compactOp = new CompactDatabaseOperation($settings);
+
+            // Execute compaction by passing the operation to Maintenance.Server.Send
+            /** @var Operation $operation */
+            $operation = $documentStore->maintenance()->server()->send($compactOp);
+            // Wait for operation to complete
+            $operation->waitForCompletion();
+            #endregion
+            } finally {
+                $documentStore->close();
+            }
+
+
+        $documentStore = new DocumentStore();
+        try {
+            #region compact_2
+            // Get all indexes names in the database using the 'GetIndexNamesOperation' operation
+            // Use 'ForDatabase' if the target database is different than the default database defined on the store
+            /** @var StringArrayResult $allIndexNames */
+            $allIndexNames = $documentStore->maintenance()->forDatabase("Northwind")
+                    ->send(new GetIndexNamesOperation(0, PhpClient::INT_MAX_VALUE));
+
+            // Define the compact settings
+            $settings = new CompactSettings();
+            $settings->setDatabaseName("Northwind");    // Database to compact
+            $settings->setDocuments(true);              // Compact all documents
+            $settings->setIndexes($allIndexNames->getArrayCopy());      // All indexes will be compacted
+            $settings->setSkipOptimizeIndexes(true);    // Skip Lucene's indexes optimization
+
+            // Define the compact operation, pass the settings
+            /** @var OperationIdResult $compactOp */
+            $compactOp = new CompactDatabaseOperation($settings);
+
+            // Execute compaction by passing the operation to Maintenance.Server.Send
+            /** @var Operation $operation */
+            $operation = $documentStore->maintenance()->server()->send($compactOp);
+
+            // Wait for operation to complete
+            $operation->waitForCompletion();
+            #endregion
+        } finally {
+            $documentStore->close();
+        }
+
+        $documentStore = new DocumentStore();
+        try {
+            #region compact_3
+            // Get all member nodes in the database-group using the 'GetDatabaseRecordOperation' operation
+            /** @var DatabaseRecordWithEtag $databaseRecord */
+            $databaseRecord = $documentStore->maintenance()->server()->send(new GetDatabaseRecordOperation("Northwind"));
+
+            $allMemberNodes = $databaseRecord->getTopology()->getMembers();
+
+            // Define the compact settings as needed
+            $settings = new CompactSettings();
+
+            $settings->setDatabaseName("Northwind");
+            $settings->setDocuments(true); //Compact all documents in database
+
+            // Execute the compact operation on each member node
+            foreach ($allMemberNodes as $nodeTag) {
+                // Define the compact operation, pass the settings
+                /** @var OperationIdResult $compactOp */
+                $compactOp = new CompactDatabaseOperation($settings);
+
+                // Execute the operation on a specific node
+                // Use `ForNode` to specify the node to operate on
+                /** @var Operation $operation */
+                $operation = $documentStore->maintenance()->server()->forNode($nodeTag)->send($compactOp);
+                // Wait for operation to complete
+                $operation->waitForCompletion();
+            }
+            #endregion
+        } finally {
+            $documentStore->close();
+        }
     }
 }
 
